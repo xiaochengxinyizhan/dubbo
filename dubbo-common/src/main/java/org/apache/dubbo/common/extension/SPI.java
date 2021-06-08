0c9858012a4b914c2a2b37886f4f1f04b6c16625
liuxiaocheng <liuxiaocheng@didiglobal.com>
2021-06-08 17:44:46 +0800

    add the SPI extension's comment

--- dubbo-common/src/main/java/org/apache/dubbo/common/extension/SPI.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/extension/SPI.java
@@ -25,9 +25,12 @@ import java.lang.annotation.Target;
 
 /**
  * Marker for extension interface
+ * 扩展接口标识器
  * <p/>
  * Changes on extension configuration file <br/>
+ * 在扩展配置文件中的更改
  * Use <code>Protocol</code> as an example, its configuration file 'META-INF/dubbo/com.xxx.Protocol' is changed from: <br/>
+ * 使用<code>Protocol</code>作为一个例子，他的配置文件为'META-INF/dubbo/com.xxx.Protocol'改变从com.foo.XxxProtocol变为KV键值对xxx=com.foo.XxxProtocol。
  * <pre>
  *     com.foo.XxxProtocol
  *     com.foo.YyyProtocol
@@ -40,15 +43,20 @@ import java.lang.annotation.Target;
  * </pre>
  * <br/>
  * The reason for this change is:
+ * 这个改变的原因是
  * <p>
  * If there's third party library referenced by static field or by method in extension implementation, its class will
  * fail to initialize if the third party library doesn't exist. In this case, dubbo cannot figure out extension's id
  * therefore cannot be able to map the exception information with the extension, if the previous format is used.
+ * 如果这里有第三方库引用通过静态属性或者扩展实现的方法，他的类将不可以被初始化，如果第三方库不存在。
+ * 为了防止dubbo不能指出扩展的ID这个问题。如果使用之前的格式，则不能够映射这个扩展的异常信息。
  * <p/>
  * For example:
+ * 举个例子：
  * <p>
  * Fails to load Extension("mina"). When user configure to use mina, dubbo will complain the extension cannot be loaded,
  * instead of reporting which extract extension implementation fails and the extract reason.
+ * 失败加载了扩展("mina")。当使用者配置使用mina，dubbo将因为不能加载这个扩展，而不是报告提取哪个扩展实现失败了以及提取的失败原因。
  * </p>
  */
 @Documented
@@ -58,6 +66,7 @@ public @interface SPI {
 
     /**
      * default extension name
+     * 默认的扩展名字
      */
     String value() default "";
 
--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Filter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Filter.java
@@ -20,12 +20,15 @@ import org.apache.dubbo.common.extension.SPI;
 
 /**
  * Extension for intercepting the invocation for both service provider and consumer, furthermore, most of
- * 扩展
  * functions in dubbo are implemented base on the same mechanism. Since every time when remote method is
  * invoked, the filter extensions will be executed too, the corresponding penalty should be considered before
  * more filters are added.
+ * 为了拦截服务的提供者和消费者执行的扩展，再者，在dubbo中的大多数函数功能都是基于同样机制实现。
+ * 因为每次当远程方法被执行的时候，过滤器的扩展也将被执行，相应的拦截应该在更多过滤器添加之前被识别。
  * <pre>
  *  They way filter work from sequence point of view is
+ *  从顺序来看，他们的工作方式为如下：
+ *
  *    <b>
  *    ...code before filter ...
  *          invoker.invoke(invocation) //filter work in a filter implementation class
@@ -33,8 +36,10 @@ import org.apache.dubbo.common.extension.SPI;
  *    </b>
  *    Caching is implemented in dubbo using filter approach. If cache is configured for invocation then before
  *    remote call configured caching type's (e.g. Thread Local, JCache etc) implementation invoke method gets called.
+ *    缓存是在dubbo使用filter方法被实现的。如果缓存为执行方式配置，那么在远程调用配置前缓存类型（比如：Thread Local, JCache等）实现方法将被调用。
  * </pre>
  * Filter. (SPI, Singleton, ThreadSafe)
+ * Filter（SPI全称Service Provider Interface，单例，线程安全）
  *
  * @see org.apache.dubbo.rpc.filter.GenericFilter
  * @see org.apache.dubbo.rpc.filter.EchoFilter
